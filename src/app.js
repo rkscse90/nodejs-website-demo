a9e1dc80565ddd6e98fb1737b1d95876f76e194c
Rahul Singh <rkscse90@gmail.com>
2023-05-08 10:55:46 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,58 @@
 const express=require('express')
 const hbs=require('hbs')
+const mongoose=require('mongoose')
 
 const app=express()
 
 const routes=require('./routes/main')
+const Detail=require('./models/Detail')
+
+//static/css/style.css
+app.use('/static',express.static('public'))
 app.use('/app',routes)
 
 //(template engine)
 app.set('view engine','hbs')
 app.set('views','views')
+hbs.registerPartials('views/partials')
+
+//DB Connection
+mongoose.connect(
+    'mongodb://127.0.0.1/website-demo',
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  ).then(()=>{
+    console.log('DB Successfully Connected')
+  /*  Detail.create({
+      brandName:'Arth Tech Solution',
+      brandIconUrl:'http://localhost:4567/static/images/Logo.jpg',
+      links: [
+        {
+          label:"Home",
+          url:"/"
+        },
+        {
+          label:"Serevices",
+          url:"/services"
+        },
+        {
+          label:"Gallery",
+          url:"/gallery"
+        },
+        {
+          label:"About",
+          url:"/about"
+        },
+        {
+          label:"Contact Us",
+          url:"/contactus"
+        },
+      ]
+    })*/
+  })
+  .catch((err)=>{
+    console.error('Failed to connect->'+err)
+  });
+  
 
 app.listen(process.env.PORT | 4567,()=>{
     console.log("Server started at PORT: 4567")
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,9 @@
 const express=require('express')
-
 const routes=express.Router()
 
+const Detail=require('../models/Detail')
 
+const details=Detail.findOne({'_id':'645869421adaaf87b71950b4'})
 
 routes.get('/getMessage', (req,res)=>{
     res.render('index')
